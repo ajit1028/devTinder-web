3f91d38a9663d8158dd485633fa8f0575ecf8795
ajit <ajeets@1028.com>
2024-12-23 12:08:10 +0530

    redux login name and pic shows

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("Ajit123@gmail.com");
   const [password, setPassword] = useState("Ajit@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:7777/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+
+      //console.log(res.data);
+      dispatch(addUser(res.data));
+      return navigate("/");
     } catch (error) {
       console.error(error);
     }
